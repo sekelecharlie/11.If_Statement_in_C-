3b5f2386aa6aa7188bd8593f6a6017beb597bf6b
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-13 07:59:25 +0200

    Add project files.

---
+++ 11.If_Statement_in_C#/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.If_Statement_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+
+            if (!isMale)
+            {
+                Console.WriteLine("Female");
+            }
+            else
+            {
+                Console.WriteLine("Male");
+            }
+            bool isTall = false;
+            if (!isTall && isMale )
+            {
+                Console.WriteLine("You are both female and tall ");
+            }
+            else if (!isTall && !isMale )
+            {
+                Console.WriteLine("you are neigther a male tall ");
+            }
+
+            Console.Read();
+
+        }
+    }
+}
